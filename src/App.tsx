78d092c8d538072d2a83f04809a902ba265aad99
felipemaifredo <felipemaifredo@gmail.com>
2023-10-18 16:11:28 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -6,60 +6,59 @@ import { MdOutlineTextRotateVertical } from "react-icons/md";
 import { MdOutlineTextRotateUp } from "react-icons/md";
 
 function App() {
-  const InputImage = useRef(null);
+  const inputImageRef = useRef<HTMLImageElement | null>(null);
   const [buttonClicked, setButtonClicked] = useState(0);
-  const [brilho, setBrilho] = useState('100');
-  const [contraste, setContraste] = useState('100');
-  const [saturacao, setSaturacao] = useState('100');
-  const [cinza, setCinza] = useState('0');
-  const [invercao, setIvercao] = useState('0');
   const [imagemURL, setImagemURL] = useState('');
+
+  const [imageFilters, setImageFilters] = useState({
+    brilho: '100',
+    contraste: '100',
+    saturacao: '100',
+    cinza: '0',
+    invercao: '0',
+  });
 
   const handleButtonClick = (buttonIndex: number) => {
     setButtonClicked(buttonIndex);
   };
 
-  const changeBrilho = (e: ChangeEvent<HTMLInputElement>) => {
-    let valueInput = e.target.value;
-    setBrilho(valueInput)
+  const changeProperty = (e: ChangeEvent<HTMLInputElement>) => {
+    setImageFilters({ ...imageFilters, [e.target.name]: e.target.value });
+    attImage();
   }
 
-  const changeContraste = (e: ChangeEvent<HTMLInputElement>) => {
-    let valueInput = e.target.value;
-    setContraste(valueInput)
-  }
-
-  const changeSaturacao = (e: ChangeEvent<HTMLInputElement>) => {
-    let valueInput = e.target.value;
-    setSaturacao(valueInput)
-  }
-
-  const changeCinza = (e: ChangeEvent<HTMLInputElement>) => {
-    let valueInput = e.target.value;
-    setCinza(valueInput)
-  }
-
-  const changeInvercao = (e: ChangeEvent<HTMLInputElement>) => {
-    let valueInput = e.target.value;
-    setIvercao(valueInput)
-  }
-
-  const handleImagem1Selecionada = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleImagemSelecionada = (e: ChangeEvent<HTMLInputElement>) => {
     const file = (e.target as HTMLInputElement).files?.[0];
     if (file) {
-      setImagemURL(URL.createObjectURL(file))
+      setImagemURL(URL.createObjectURL(file));
+      cleamFilters();
     } else {
       setImagemURL('');
-    }
+    };
   };
 
   const cleamFilters = () => {
-    setBrilho('100')
-    setContraste('100')
-    setSaturacao('100')
-    setCinza('0')
-    setIvercao('0')
-  }
+    setImageFilters({
+      brilho: '100',
+      contraste: '100',
+      saturacao: '100',
+      cinza: '0',
+      invercao: '0',
+    });
+    attImage();
+  };
+
+  function attImage() {
+    if (inputImageRef.current) {
+      inputImageRef.current.style.filter = `
+        brightness(${imageFilters.brilho}%) 
+        contrast(${imageFilters.contraste}%) 
+        saturate(${imageFilters.saturacao}%) 
+        grayscale(${imageFilters.cinza}%) 
+        invert(${imageFilters.invercao}%)
+      `;
+    }
+  };
 
   return (
     <div className="App">
@@ -68,12 +67,12 @@ function App() {
           <button onClick={cleamFilters}>Limpar Filtros</button>
           <label>
             <span> Nova Imagem </span>
-            <input ref={InputImage} id='input-file' type="file" onChange={handleImagem1Selecionada} />
+            <input id='input-file' type="file" onChange={handleImagemSelecionada} />
           </label>
           <button>Salvar</button>
         </div>
         <div className='image-container'>
-          {imagemURL && ( <img src={imagemURL} alt='Imagem para edição' /> )}
+          {imagemURL && ( <img src={imagemURL} alt='Imagem para edição' ref={inputImageRef} /> )}
         </div>
         <div className='rotate-btns-container'>
           <button> <AiOutlineRotateLeft /> </button>
@@ -87,32 +86,32 @@ function App() {
               <div className="range-inputs-box">
                 {buttonClicked === 0 && (
                   <>
-                    <p>{brilho}</p>          
-                    <input type="range" name='brilho' min="0" max="200" value={brilho} onChange={(e) => {changeBrilho(e)}}/>
+                    <p>{imageFilters.brilho}</p>          
+                    <input type="range" name='brilho' min="0" max="200" value={imageFilters.brilho} onChange={(e) => {changeProperty(e)}}/>
                   </>
                 )}
                 {buttonClicked === 1 && (
                   <>
-                    <p>{contraste}</p> 
-                    <input type="range" name='brilho' min="0" max="200" value={contraste} onChange={(e) => {changeContraste(e)}}/>
+                    <p>{imageFilters.contraste}</p> 
+                    <input type="range" name='contraste' min="0" max="200" value={imageFilters.contraste} onChange={(e) => {changeProperty(e)}}/>
                   </>
                 )}
                 {buttonClicked === 2 && (
                   <>
-                    <p>{saturacao}</p> 
-                    <input type="range" name='brilho' min="0" max="200" value={saturacao} onChange={(e) => {changeSaturacao(e)}}/>
+                    <p>{imageFilters.saturacao}</p> 
+                    <input type="range" name='saturacao' min="0" max="200" value={imageFilters.saturacao} onChange={(e) => {changeProperty(e)}}/>
                   </>
                 )}
                 {buttonClicked === 3 && (
                   <>
-                    <p>{cinza}</p> 
-                    <input type="range" name='brilho' min="0" max="100" value={cinza} onChange={(e) => {changeCinza(e)}}/>
+                    <p>{imageFilters.cinza}</p> 
+                    <input type="range" name='cinza' min="0" max="100" value={imageFilters.cinza} onChange={(e) => {changeProperty(e)}}/>
                   </>
                 )}
                 {buttonClicked === 4 && (
                   <>
-                    <p>{invercao}</p> 
-                    <input type="range" name='brilho' min="0" max="100" value={invercao} onChange={(e) => {changeInvercao(e)}}/>
+                    <p>{imageFilters.invercao}</p> 
+                    <input type="range" name='invercao' min="0" max="100" value={imageFilters.invercao} onChange={(e) => {changeProperty(e)}}/>
                   </>
                 )}
               </div>
