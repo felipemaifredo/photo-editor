fd32b9725cf34796b8cf6d9ebb5ebef64972c558
felipemaifredo <felipemaifredo@gmail.com>
2023-11-14 16:40:16 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -60,71 +60,85 @@ function App() {
     }
   };
 
+  function downloadImage() {
+    alert('opa')
+  }
+
   return (
     <div className="App">
       <div className='editor-container'>
         <div className='bts-container'>
-          <button onClick={cleamFilters}>Limpar Filtros</button>
+          {imagemURL && 
+            <button onClick={cleamFilters}>Limpar Filtros</button>
+          }
           <label>
             <span> Nova Imagem </span>
             <input id='input-file' type="file" onChange={handleImagemSelecionada} />
           </label>
-          <button>Salvar</button>
+          {imagemURL && 
+            <button onClick={downloadImage}>Salvar</button>
+          }
         </div>
-        <div className='image-container'>
-          {imagemURL && ( <img src={imagemURL} alt='Imagem para edição' ref={inputImageRef} /> )}
-        </div>
-        <div className='rotate-btns-container'>
-          <button> <AiOutlineRotateLeft /> </button>
-          <button> <AiOutlineRotateRight /> </button>
-          <button> <MdOutlineTextRotateVertical /> </button>
-          <button> <MdOutlineTextRotateUp /> </button>
-        </div>
-        <div className='controls-container'>
-          <div className='range-inputs-container'>
-            {buttonClicked !== null && (
-              <div className="range-inputs-box">
-                {buttonClicked === 0 && (
-                  <>
-                    <p>{imageFilters.brilho}</p>          
-                    <input type="range" name='brilho' min="0" max="200" value={imageFilters.brilho} onChange={(e) => {changeProperty(e)}}/>
-                  </>
-                )}
-                {buttonClicked === 1 && (
-                  <>
-                    <p>{imageFilters.contraste}</p> 
-                    <input type="range" name='contraste' min="0" max="200" value={imageFilters.contraste} onChange={(e) => {changeProperty(e)}}/>
-                  </>
-                )}
-                {buttonClicked === 2 && (
-                  <>
-                    <p>{imageFilters.saturacao}</p> 
-                    <input type="range" name='saturacao' min="0" max="200" value={imageFilters.saturacao} onChange={(e) => {changeProperty(e)}}/>
-                  </>
-                )}
-                {buttonClicked === 3 && (
-                  <>
-                    <p>{imageFilters.cinza}</p> 
-                    <input type="range" name='cinza' min="0" max="100" value={imageFilters.cinza} onChange={(e) => {changeProperty(e)}}/>
-                  </>
-                )}
-                {buttonClicked === 4 && (
-                  <>
-                    <p>{imageFilters.invercao}</p> 
-                    <input type="range" name='invercao' min="0" max="100" value={imageFilters.invercao} onChange={(e) => {changeProperty(e)}}/>
-                  </>
-                )}
-              </div>
-            )}
+        {imagemURL &&
+          <div className='image-container'>
+            <img src={imagemURL} alt='Imagem para edição' ref={inputImageRef} />
           </div>
-          <div className="select-input-btns-container">
-            <button className={buttonClicked === 0 ? 'active' : ''} onClick={() => handleButtonClick(0)} > Brilho </button> 
-            <button className={buttonClicked === 1 ? 'active' : ''} onClick={() => handleButtonClick(1)} > Contraste </button>
-            <button className={buttonClicked === 2 ? 'active' : ''} onClick={() => handleButtonClick(2)} > Saturação </button>     
-            <button className={buttonClicked === 3 ? 'active' : ''} onClick={() => handleButtonClick(3)} > Cinza </button>
-            <button className={buttonClicked === 4 ? 'active' : ''} onClick={() => handleButtonClick(4)} > Inversão </button>
+         }
+        {imagemURL && 
+          <div className='rotate-btns-container'>
+            <button> <AiOutlineRotateLeft /> </button>
+            <button> <AiOutlineRotateRight /> </button>
+            <button> <MdOutlineTextRotateVertical /> </button>
+            <button> <MdOutlineTextRotateUp /> </button>
           </div>
-        </div>
+        }
+        {imagemURL && 
+          <div className='controls-container'>
+            <div className='range-inputs-container'>
+              {buttonClicked !== null && (
+                <div className="range-inputs-box">
+                  {buttonClicked === 0 && (
+                    <>
+                      <p>{imageFilters.brilho}</p>          
+                      <input type="range" name='brilho' min="0" max="200" value={imageFilters.brilho} onChange={(e) => {changeProperty(e)}}/>
+                    </>
+                  )}
+                  {buttonClicked === 1 && (
+                    <>
+                      <p>{imageFilters.contraste}</p> 
+                      <input type="range" name='contraste' min="0" max="200" value={imageFilters.contraste} onChange={(e) => {changeProperty(e)}}/>
+                    </>
+                  )}
+                  {buttonClicked === 2 && (
+                    <>
+                      <p>{imageFilters.saturacao}</p> 
+                      <input type="range" name='saturacao' min="0" max="200" value={imageFilters.saturacao} onChange={(e) => {changeProperty(e)}}/>
+                    </>
+                  )}
+                  {buttonClicked === 3 && (
+                    <>
+                      <p>{imageFilters.cinza}</p> 
+                      <input type="range" name='cinza' min="0" max="100" value={imageFilters.cinza} onChange={(e) => {changeProperty(e)}}/>
+                    </>
+                  )}
+                  {buttonClicked === 4 && (
+                    <>
+                      <p>{imageFilters.invercao}</p> 
+                      <input type="range" name='invercao' min="0" max="100" value={imageFilters.invercao} onChange={(e) => {changeProperty(e)}}/>
+                    </>
+                  )}
+                </div>
+              )}
+            </div>
+            <div className="select-input-btns-container">
+              <button className={buttonClicked === 0 ? 'active' : ''} onClick={() => handleButtonClick(0)} > Brilho </button> 
+              <button className={buttonClicked === 1 ? 'active' : ''} onClick={() => handleButtonClick(1)} > Contraste </button>
+              <button className={buttonClicked === 2 ? 'active' : ''} onClick={() => handleButtonClick(2)} > Saturação </button>     
+              <button className={buttonClicked === 3 ? 'active' : ''} onClick={() => handleButtonClick(3)} > Cinza </button>
+              <button className={buttonClicked === 4 ? 'active' : ''} onClick={() => handleButtonClick(4)} > Inversão </button>
+            </div>
+          </div>
+        }
       </div>
     </div>
   );
