dd2d27ff8f98979404cc4fb92cae7f0eb85d9ce8
felipemaifredo <felipemaifredo@gmail.com>
2023-12-05 19:27:20 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css';
-import { useState, ChangeEvent, useRef } from 'react';
+import { useState, ChangeEvent, useRef, MutableRefObject } from 'react';
 import { AiOutlineRotateRight } from "react-icons/ai";
 import { AiOutlineRotateLeft } from "react-icons/ai";
 import { MdOutlineTextRotateVertical } from "react-icons/md";
 import { MdOutlineTextRotateUp } from "react-icons/md";
 
 function App() {
-  const inputImageRef = useRef<HTMLImageElement | null>(null);
+  const inputImageRef: MutableRefObject<HTMLImageElement | null> = useRef(null);
   const [buttonClicked, setButtonClicked] = useState(0);
   const [imagemURL, setImagemURL] = useState('');
+  const [rotacaoAtual, setRotacaoAtual] = useState<number>(0);
+  const [XAtual, setXAtual] = useState<number>(1);
+  const [YAtual, setYAtual] = useState<number>(1);
 
   const [imageFilters, setImageFilters] = useState({
     brilho: '100',
@@ -25,7 +28,7 @@ function App() {
   const changeProperty = (e: ChangeEvent<HTMLInputElement>) => {
     setImageFilters({ ...imageFilters, [e.target.name]: e.target.value });
     attImage();
-  }
+  };
 
   const handleImagemSelecionada = (e: ChangeEvent<HTMLInputElement>) => {
     const file = (e.target as HTMLInputElement).files?.[0];
@@ -37,6 +40,65 @@ function App() {
     };
   };
 
+  const rotate90 = (guidance: boolean) => {
+    let newRotacao;
+    
+    if (guidance) {
+      newRotacao = rotacaoAtual + 90;
+      setRotacaoAtual(newRotacao);
+      if (newRotacao === 360) {
+        setRotacaoAtual(0);
+      }
+
+    } else {
+      newRotacao = rotacaoAtual - 90;
+      setRotacaoAtual(newRotacao);
+      if (newRotacao === -360) {
+        setRotacaoAtual(0);
+      }
+    }
+
+    // Construa a expressão de rotação usando a nova rotação
+    let rotateExpression = `rotate(${newRotacao}deg)`;
+
+    // Aplique a transformação ao seu elemento (substitua "elementRef" pelo seu ref)
+    if (inputImageRef.current) {
+      inputImageRef.current.style.transform = rotateExpression;
+    }
+  };
+
+  const translateX = () => {
+    // Atualiza o estado com o novo valor de XAtual
+    setXAtual((prevXAtual) => (prevXAtual === 1 ? -1 : 1));
+  
+    // Obtém o valor atualizado de XAtual
+    const newXAtual = XAtual === 1 ? -1 : 1;
+  
+    // Construa a expressão de escala usando o novo valor de XAtual e o valor atual de YAtual
+    const scaleXExpression = `scaleX(${newXAtual}) scaleY(${YAtual})`;
+  
+    // Aplique a transformação ao seu elemento (substitua "inputImageRef" pelo seu ref)
+    if (inputImageRef.current) {
+      inputImageRef.current.style.transform = scaleXExpression;
+    }
+  };
+  
+  const translateY = () => {
+    // Atualiza o estado com o novo valor de YAtual
+    setYAtual((prevYAtual) => (prevYAtual === 1 ? -1 : 1));
+  
+    // Obtém o valor atualizado de YAtual
+    const newYAtual = YAtual === 1 ? -1 : 1;
+  
+    // Construa a expressão de escala usando o valor atual de XAtual e o novo valor de YAtual
+    const scaleYExpression = `scaleX(${XAtual}) scaleY(${newYAtual})`;
+  
+    // Aplique a transformação ao seu elemento (substitua "inputImageRef" pelo seu ref)
+    if (inputImageRef.current) {
+      inputImageRef.current.style.transform = scaleYExpression;
+    }
+  };
+  
   const cleamFilters = () => {
     setImageFilters({
       brilho: '100',
@@ -45,6 +107,11 @@ function App() {
       cinza: '0',
       invercao: '0',
     });
+
+    if (inputImageRef.current) {
+      inputImageRef.current.style.transform = 'scaleX(1) scaleY(1)';
+    }
+
     attImage();
   };
 
@@ -60,9 +127,64 @@ function App() {
     }
   };
 
-  function downloadImage() {
-    alert('opa')
+  async function downloadImage() {
+    // Verifica se há uma imagem carregada
+    if (imagemURL && inputImageRef.current) {
+      // Cria um canvas para desenhar a imagem com as configurações aplicadas
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+  
+      // Carrega a imagem original no elemento de imagem
+      const img = inputImageRef.current;
+  
+      // Ajusta o tamanho do canvas para o tamanho da imagem
+      canvas.width = img.width;
+      canvas.height = img.height;
+  
+      // Aplica as transformações no contexto do canvas
+      const rotate = rotacaoAtual;
+      const flipX = XAtual;
+      const flipY = YAtual;
+  
+      ctx?.translate(canvas.width / 2, canvas.height / 2);
+      if (rotate !== 0) ctx?.rotate((rotate * Math.PI) / 180);
+  
+      // Ajusta a escala separadamente para X e Y
+      ctx?.scale(flipX, flipY);
+  
+      // Aplica os filtros diretamente no contexto do canvas
+      if (ctx) {
+        ctx.filter = `
+        brightness(${imageFilters.brilho}%) 
+        contrast(${imageFilters.contraste}%) 
+        saturate(${imageFilters.saturacao}%) 
+        grayscale(${imageFilters.cinza}%) 
+        invert(${imageFilters.invercao}%)
+      `;
+      }
+      
+      // Desenha a imagem com todas as transformações no canvas
+      ctx?.drawImage(img, -canvas.width / 2, -canvas.height / 2);
+  
+      // Cria uma imagem base64 da imagem no canvas
+      const base64Image = canvas.toDataURL("image/png");
+  
+      // Cria um link para download
+      const downloadLink = document.createElement("a");
+      downloadLink.href = base64Image;
+      downloadLink.download = "imagem_editada.png";
+  
+      // Adiciona o link ao corpo do documento
+      document.body.appendChild(downloadLink);
+  
+      // Simula um clique no link para iniciar o download
+      downloadLink.click();
+  
+      // Remove o link do corpo do documento
+      document.body.removeChild(downloadLink);
+    }
   }
+  
 
   return (
     <div className="App">
@@ -86,10 +208,10 @@ function App() {
          }
         {imagemURL && 
           <div className='rotate-btns-container'>
-            <button> <AiOutlineRotateLeft /> </button>
-            <button> <AiOutlineRotateRight /> </button>
-            <button> <MdOutlineTextRotateVertical /> </button>
-            <button> <MdOutlineTextRotateUp /> </button>
+            <button onClick={() => rotate90(false)}> <AiOutlineRotateLeft /> </button>
+            <button onClick={() => rotate90(true)}> <AiOutlineRotateRight /> </button>
+            <button onClick={translateX} > <MdOutlineTextRotateVertical /> </button>
+            <button onClick={translateY} > <MdOutlineTextRotateUp /> </button>
           </div>
         }
         {imagemURL && 
